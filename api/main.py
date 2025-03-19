98f8903b1ea42c26a400fec58788dc09cf21f7fe
bijan_pradhananga <bijan2059@gmail.com>
2025-03-19 14:57:02 +0545

    added recommend function

--- api/main.py
+++ api/main.py
@@ -1,7 +1,28 @@
-from fastapi import FastAPI, HTTPException
-from api.services.recommendation_service import get_similar_phones
+from fastapi import FastAPI, HTTPException, Query
+from pydantic import BaseModel, Field
+from typing import List
+from api.services.recommendation_service import get_similar_phones, get_top_recommendations, prepare_user_data
 
 app = FastAPI()
+
+class PurchasedProduct(BaseModel):
+    product: str
+
+class Rating(BaseModel):
+    product: str
+    rating: float = Field(..., ge=1, le=5)  # Ensuring rating is a float between 1 and 5
+    review: str = ""
+
+class UserActivity(BaseModel):
+    user: str
+    purchasedProducts: List[PurchasedProduct] = []
+    ratings: List[Rating] = []
+    viewedProducts: List[str] = []
+
+    class Config:
+        json_encoders = {
+            float: lambda v: float(v)  # Ensure ratings are treated as floats
+        }
 
 @app.get("/")
 async def hello():
@@ -13,3 +34,16 @@ async def similar_phones(phone_model: str, top_n: int = 4):
     if "error" in result:
         raise HTTPException(status_code=404, detail=result["error"])
     return result
+
+@app.post("/recommendations/")
+async def recommendations(user_activity: UserActivity,top_n: int = Query(default=4)):
+    """
+    Endpoint to get collaborative filtering recommendations.
+    """
+    # Prepare user data
+    df = prepare_user_data(user_activity.dict())
+
+    # Get top recommendations
+    recommendations = get_top_recommendations(user_activity.user, df, top_n)
+    return {"user_id": user_activity.user, "recommendations": recommendations}
+
